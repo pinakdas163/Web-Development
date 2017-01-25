a94a21aa029040d16779a0f4b6ce1a2eeeff4aca
Pinak Das <pinakdas163@gmail.com>
2017-01-24 23:57:54 -0800

    view engine addition

--- NodeJs/newserver.js
+++ NodeJs/newserver.js
@@ -1,7 +1,7 @@
 var express= require('express');
 
 var app= express();
-
+app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
   res.sendFile(__dirname+'/page.html');
 });
@@ -11,7 +11,8 @@ app.get('/contact', function(req, res) {
 });
 
 app.get('/profile/:name', function(req, res) {
-  res.send('This is a profile page of '+req.params.name);
+  var data={age:26, hobbies:['movies', 'exercise', 'learning']};
+  res.render('profile', {person:req.params.name, data:data});
 });
 
 app.listen(3000);
